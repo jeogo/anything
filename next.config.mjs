0844ee36cad06f6ffec14e421d3dd95feb497907
jeogo <65985256+jeogo@users.noreply.github.com>
2024-08-26 01:26:52 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'], // Specify custom page extensions if your files use custom names inside src
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
@@ -9,6 +10,10 @@ const nextConfig = {
     }
     return config;
   },
+  // Specify that the base directory for your application is 'src'
+  basePath: '/src',
+  // Specify the directory where pages are located (this is optional if your pages are inside `src/pages`)
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
 export default nextConfig;
